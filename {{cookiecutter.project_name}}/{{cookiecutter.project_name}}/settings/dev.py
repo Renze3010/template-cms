c31ae1d3c44f8ec606fcc666eebe7ae385407a1b
Renze3010 <rjboerman@ziggo.nl>
2022-05-17 22:50:09 +0200

    initial commit

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!uhi&q$)#_5!obhxn)8x_au31&fk5(^3ke#70zh2#-57!-auc-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
